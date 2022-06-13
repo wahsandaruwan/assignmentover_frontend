bddf98b8850e251ddd96b3e217179471d59b84fd
wahsandaruwan <wahsandaruwan6@gmail.com>
2022-06-13 18:06:17 +0530

    Services page updated

--- src/Components/Pages/Services.js
+++ src/Components/Pages/Services.js
@@ -1,22 +1,32 @@
 import MainButton from '../Elements/MainButton'
 import ServiceCard from '../Sections/ServiceCard'
 import serviceData from '../../Data/Services.json'
+import { useState } from 'react'
 
 const Services = () => {
+  const [serviceType, setServiceType] = useState('Assignments')
   return (
     <>
       <div className="page services">
         <h1 className="heading">Our Services</h1>
         <div className="tabs">
-          <MainButton buttonText="Assignment Services" extraClass="active" />
+          <MainButton
+            buttonText="Assignment Services"
+            clickFunc={() => setServiceType('Assignments')}
+            extraClass="active"
+          />
           <MainButton buttonText="Research Services" />
           <MainButton buttonText="Other Services" />
         </div>
         <div className="services-grid">
           <div className="assignments">
-            {serviceData.map((data, index) => {
-              const { id, title, details } = data
-              return <ServiceCard key={id} title={title} details={details} />
+            {serviceData[0].body.map((data) => {
+              const { id, title, details, type } = data
+              return (
+                type === 'Assignments' && (
+                  <ServiceCard key={id} title={title} details={details} />
+                )
+              )
             })}
           </div>
         </div>
