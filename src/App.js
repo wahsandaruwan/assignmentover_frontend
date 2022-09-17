49060d97ab66d95335500943b90c07e5dd974607
wahsandaruwan <wahsandaruwan6@gmail.com>
2022-09-17 18:09:37 +0530

    Updated padding in pages

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 // Third-party components & modules
-import { Routes, Route, useLocation } from 'react-router-dom'
-import { FaWhatsappSquare } from 'react-icons/fa'
+import { Routes, Route, useLocation } from "react-router-dom";
+import { FaWhatsappSquare } from "react-icons/fa";
 
 // Custom components & modules
-import About from './Components/Pages/About'
-import Contact from './Components/Pages/Contact'
-import Features from './Components/Pages/Features'
-import Home from './Components/Pages/Home'
-import Services from './Components/Pages/Services'
-import Testimonials from './Components/Pages/Testimonials'
-import Footer from './Components/Sections/Footer'
-import NavBar from './Components/Sections/NavBar'
-import { openWhatsAppClient } from './Helpers/OpenClients'
-import Dashboard from './Components/Pages/Dashboard'
-import Login from './Components/Pages/Login'
+import About from "./Components/Pages/About";
+import Contact from "./Components/Pages/Contact";
+import Features from "./Components/Pages/Features";
+import Home from "./Components/Pages/Home";
+import Services from "./Components/Pages/Services";
+import Testimonials from "./Components/Pages/Testimonials";
+import Footer from "./Components/Sections/Footer";
+import NavBar from "./Components/Sections/NavBar";
+import { openWhatsAppClient } from "./Helpers/OpenClients";
+import Dashboard from "./Components/Pages/Dashboard";
+import Login from "./Components/Pages/Login";
 
 const App = () => {
   // Current route information
-  const location = useLocation()
+  const location = useLocation();
 
   return (
     <>
-      {location.pathname !== '/login' && location.pathname !== '/dashboard' ? (
+      {location.pathname !== "/login" && location.pathname !== "/dashboard" ? (
         <NavBar />
       ) : (
-        ''
+        ""
       )}
       <Routes>
         <Route path="/" element={<Home />} />
@@ -36,7 +36,7 @@ const App = () => {
         <Route path="login" element={<Login />} />
         <Route path="dashboard" element={<Dashboard />} />
       </Routes>
-      {location.pathname !== '/login' && location.pathname !== '/dashboard' ? (
+      {location.pathname !== "/login" && location.pathname !== "/dashboard" ? (
         <>
           <Footer />
           <div className="fixed-whatsapp-btn">
@@ -44,10 +44,10 @@ const App = () => {
           </div>
         </>
       ) : (
-        ''
+        ""
       )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
